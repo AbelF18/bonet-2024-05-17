df826748ac233ce12f2bfa342f266257fa909937
AbelF18 <abelbonet2004@ginebro.cat>
2024-05-23 17:08:51 +0200

    ok

--- src/Pilota.java
+++ src/Pilota.java
@@ -1,9 +1,18 @@
 public class Pilota {
     private String nomPropietari;{
-        this.nomPropietariRebut;
+        this.nomPropietari = nomPropietariRebut;
+            this.nomPropietari = nomPropietariRebut;
+    }
+    public String getNomPropietari() {
+
+        String nom1;
+        System.out.print("Entra el nom del propietari de la pilota: ");
+        String nom1 = teclat.nextLine();
+        return this.nomPropietari;
     }
 
+    public void setNomPropietari(String nomPropietariRebut){
 
 
-
+    }
 }
