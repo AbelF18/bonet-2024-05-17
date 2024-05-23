f73e040cb248f467e0038b5b5775ea2ecd0f959e
AbelF18 <abelbonet2004@ginebro.cat>
2024-05-23 16:10:23 +0200

    ok

--- src/BonetApp.java
+++ src/BonetApp.java
@@ -2,6 +2,6 @@ import static utils.BonetEines.*;
 
 public class BonetApp {
     public static void main(String[] args) {
-        System.out.format("Número aleatori amb negatius : %d\n" , (int) BonetAleatoriAmbNegatius());
+        System.out.format("Número aleatori amb negatius : %d\n", (int) BonetAleatoriAmbNegatius());
     }
 }
